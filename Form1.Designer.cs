f19510844f0617c23946b18cd912d20377a56066
BTConca <1512250@student.fit.hcmus.vn>
2020-10-10 14:38:26 +0700

    New

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace SimpleEncrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBoxFilename = new System.Windows.Forms.TextBox();
+            this.BrowseButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.DecryptButton = new System.Windows.Forms.Button();
+            this.EncryptButton = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(13, 117);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(523, 96);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // textBoxFilename
+            // 
+            this.textBoxFilename.Enabled = false;
+            this.textBoxFilename.Location = new System.Drawing.Point(96, 11);
+            this.textBoxFilename.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxFilename.Name = "textBoxFilename";
+            this.textBoxFilename.Size = new System.Drawing.Size(258, 20);
+            this.textBoxFilename.TabIndex = 1;
+            // 
+            // BrowseButton
+            // 
+            this.BrowseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BrowseButton.Location = new System.Drawing.Point(13, 10);
+            this.BrowseButton.Margin = new System.Windows.Forms.Padding(2);
+            this.BrowseButton.Name = "BrowseButton";
+            this.BrowseButton.Size = new System.Drawing.Size(72, 20);
+            this.BrowseButton.TabIndex = 2;
+            this.BrowseButton.Text = "Browse";
+            this.BrowseButton.UseVisualStyleBackColor = true;
+            this.BrowseButton.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Location = new System.Drawing.Point(96, 34);
+            this.textBoxPassword.Margin = new System.Windows.Forms.Padding(2);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(258, 20);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // DecryptButton
+            // 
+            this.DecryptButton.Location = new System.Drawing.Point(476, 11);
+            this.DecryptButton.Margin = new System.Windows.Forms.Padding(2);
+            this.DecryptButton.Name = "DecryptButton";
+            this.DecryptButton.Size = new System.Drawing.Size(56, 44);
+            this.DecryptButton.TabIndex = 5;
+            this.DecryptButton.Text = "Decrypt";
+            this.DecryptButton.UseVisualStyleBackColor = true;
+            this.DecryptButton.Click += new System.EventHandler(this.Decrypt_Click);
+            // 
+            // EncryptButton
+            // 
+            this.EncryptButton.Location = new System.Drawing.Point(410, 10);
+            this.EncryptButton.Margin = new System.Windows.Forms.Padding(2);
+            this.EncryptButton.Name = "EncryptButton";
+            this.EncryptButton.Size = new System.Drawing.Size(56, 44);
+            this.EncryptButton.TabIndex = 6;
+            this.EncryptButton.Text = "Encrypt";
+            this.EncryptButton.UseVisualStyleBackColor = true;
+            this.EncryptButton.Click += new System.EventHandler(this.Encrypt_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(66, 84);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(258, 20);
+            this.textBox1.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 87);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(50, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "HashKey";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(536, 224);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EncryptButton);
+            this.Controls.Add(this.DecryptButton);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BrowseButton);
+            this.Controls.Add(this.textBoxFilename);
+            this.Controls.Add(this.richTextBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Simple Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBoxFilename;
+        private System.Windows.Forms.Button BrowseButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Button DecryptButton;
+        private System.Windows.Forms.Button EncryptButton;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace SimpleEncrypt
+{
+    public partial class Form1 : Form
+    {
+        private AES _aes;
+        //private byte[] IV = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
+        //private int BlockSize = 128;
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            _aes = new AES();
+            label3.Hide();  // For Dl 2.2
+            textBox1.Hide(); // For DL 2.2
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.Cancel) return;
+            textBoxFilename.Text = openFileDialog1.FileName;
+
+            FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+            StreamReader sr = new StreamReader(fs);
+
+            richTextBox1.Text = sr.ReadToEnd();
+
+            sr.Close();
+            fs.Close();
+        }
+
+
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+            if (textBoxFilename.Text != "")
+            {
+
+                if (textBoxPassword.Text != "")
+                {
+
+                    string password = textBoxPassword.Text;
+                    string notEncryptedText = richTextBox1.Text;
+                    string encryptedText = _aes.Encrypt(password, notEncryptedText);
+                    textBox1.Text = _aes.getHashKey();
+                    richTextBox1.Text = encryptedText;
+                    using (StreamWriter writer = new StreamWriter(textBoxFilename.Text, false))
+                    {
+                        writer.Write(encryptedText);
+                    }
+                    if (MessageBox.Show("Encryption is successful, do you want to open file?", "What is your action?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        Process.Start(textBoxFilename.Text);
+                    }
+                }
+                else
+                {
+                    System.Windows.Forms.MessageBox.Show("Please enter your password!");
+                }
+            }
+            else System.Windows.Forms.MessageBox.Show("Please choose you file!");
+        }
+
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+
+            string password = textBoxPassword.Text;
+            string decryptingText = richTextBox1.Text;
+            string plainText = _aes.Decrypt(password, decryptingText);
+            if (decryptingText == "")
+            {
+                System.Windows.Forms.MessageBox.Show("There is nothing to decrypt!");
+            }
+            else if (plainText == "")
+            {
+                System.Windows.Forms.MessageBox.Show("Incorrect Password");
+            }
+            else if (plainText == null)
+            {
+                System.Windows.Forms.MessageBox.Show("This is not a encrypted file!");
+            }
+            else
+            {
+                richTextBox1.Text = plainText;
+                using (StreamWriter writer = new StreamWriter(textBoxFilename.Text, false))
+                {
+                    writer.Write(plainText);
+                }
+                if (MessageBox.Show("Decryption is successful, do you want to open file?", "What is your action?", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    Process.Start(textBoxFilename.Text);
+                }
+            }
+        }
+    }
+}
